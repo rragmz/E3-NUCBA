b1654047d69ee1ff8f1acec3e2929f606a9f0d82
Rocio <rragmz@gmail.com>
2022-06-15 00:05:53 -0300

    Ejercicio 4: localStorage + CSS

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ Vamos a utilizar el mismo array de objetos "Pizzas🍕" del desafío general ant
 */
 const $h2 = document.getElementById('h2');
 const $h4 = document.getElementById('h4');
+const $h5 = document.getElementById('h5');
+const $ingredientes = document.getElementById('ingredientes');
 const $input = document.getElementById('input');
 const $button = document.getElementById('button');
 const $img = document.getElementById('img');
@@ -19,29 +21,35 @@ class Pizza{
     constructor(ID, nombre, ingredientes, precio, image){
         this.ID = ID;
         this.nombre = nombre;
-        this.ingredientes = [];
+        this.ingredientes = ingredientes;
         this.precio = precio;
         this.image = image;
     }
 }
 
-const Fugazzeta = new Pizza(1, 'Fugazzeta', ['Muzzarella', 'Cebolla', 'Orégano'], 500, 'https://th.bing.com/th/id/OIP.9tC9PkuEonjcWf_RqL1nSgHaJQ?pid=ImgDet&rs=1');
+const Fugazzeta = new Pizza(1, 'Fugazzeta', ['Muzzarella', 'Orégano', 'Cebolla'], 500, 'https://th.bing.com/th/id/OIP.9tC9PkuEonjcWf_RqL1nSgHaJQ?pid=ImgDet&rs=1');
 const Muzzarella = new Pizza(2, 'Muzarella', ['Salsa de tomate', 'Muzzarella', 'Orégano'], 400, 'https://th.bing.com/th/id/OIP.o1aeWU2FPuTCjMI5FjbOhwHaHa?pid=ImgDet&rs=1');
 const Napolitana = new Pizza(3, 'Napolitana', ['Salsa de tomate', 'Muzzarella', 'Tomate', 'Ajo', 'Orégano'], 700, 'https://i.pinimg.com/originals/ba/1b/ff/ba1bffe2cbf53977c67712cdd16d1eb5.jpg');
 const JamonMorrones = new Pizza(4, 'Jamón y Morrones', ['Salsa de tomate', 'Muzzarella', 'Jamón', 'Morrón'], 800, 'https://th.bing.com/th/id/OIP.F3jpp3Sto5qSvTxwilQUYAHaFj?pid=ImgDet&rs=1');
 const Verdura = new Pizza(5, 'Verdura', ['Espinaca', 'Queso', 'Salsa blanca', 'Orégano'], 600, 'https://th.bing.com/th/id/OIP.KA0cS2I7kJYRqmEq2G9BxgHaFX?pid=ImgDet&rs=1');
 const Calabresa = new Pizza(6, 'Calabresa', ['Salsa de tomate', 'Muzzarella', 'Salame'], 700, 'https://th.bing.com/th/id/R.fe027c6ccaa606077437fad97a0ba009?rik=c5xRdnR9PoBthw&pid=ImgRaw&r=0');
 
+
 const Pizzas = [];
 Pizzas.push(Fugazzeta,Muzzarella,Napolitana,JamonMorrones,Verdura,Calabresa);
+//Guardo el array en el localStorage
+localStorage.setItem('Pizzas', JSON.stringify(Pizzas));
 
 const PizzasSelected=()=>{
 
     $button.addEventListener("click", (e)=>{
         
-        while($h2.firstChild && $h4.firstChild){
+        while($h2.firstChild && $h4.firstChild && $h5.firstChild){
             $h2.removeChild($h2.firstChild);
             $h4.removeChild($h4.firstChild);
+           while($h5.firstChild){
+                $h5.removeChild($h5.firstChild)
+            }
         }
 
         let inputValue = $input.value;
@@ -49,23 +57,39 @@ const PizzasSelected=()=>{
 
         
         const pizzaSelected= Pizzas.find((pizza) => pizza.ID === inputValue);
+            localStorage.setItem('Pizza seleccionada', JSON.stringify(pizzaSelected));
             if(pizzaSelected === undefined){
                 alert("No tenemos pizza con ese ID 🙁");
                 if($img){
                     $img.style.display = 'none';
                 }
+                $ingredientes.style.display = 'none';
+                $h2.style.border = 'none';
             }else{
+                $ingredientes.style.display = 'block';
                 let h2Text= document.createTextNode(pizzaSelected.nombre)
                 $h2.appendChild(h2Text);
-
+                $h2.style.border = '2px dashed black';
+                
                 let h4Text= document.createTextNode(`$${pizzaSelected.precio}`)
                 $h4.appendChild(h4Text);
-
+                
                 $img.setAttribute('src', pizzaSelected.image);
                 $img.style.display = 'inline-block';
-                $img.style.width = '350px';
-                $img.style.height = '360px';
-
+                $img.style.width = '320px';
+                $img.style.height = '330px';
+                
+                let h5text;
+                for(let i = 0; i < pizzaSelected.ingredientes.length; i++){
+                    if(i < pizzaSelected.ingredientes.length-1){
+                        h5text = document.createTextNode(`${pizzaSelected.ingredientes[i]}, `);
+                        $h5.appendChild(h5text);
+                    }
+                    else{
+                        h5text = document.createTextNode(`${pizzaSelected.ingredientes[i]}`);
+                        $h5.appendChild(h5text);
+                    }
+                }
             }
             $input.value = "";
     } )
